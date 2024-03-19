bbbe5ebb98448781a71f69544700c4bc4d88ffac
gcgbarbosa <gcgbarbosa@gmail.com>
2024-03-19 18:50:01 -0300

    Add main

---
+++ main.py
@@ -0,0 +1,16 @@
+from src.chatbot import ChatBot
+
+cb = ChatBot('TonicBot')
+
+# question = 'código atc furosemida?'
+
+question = ''
+while question != 'quit':
+    print('Type your question: ', end='')
+    question = input()
+    response = cb.respond(question)
+
+    print('#####################')
+    print('\n\n### Question:\n' +question, end='\n\n')
+    print('### Response:\n' +response)
+    print('#####################', end='\n\n')
