81bbfba35c6ef934d234f7d72f51bc788c32781e
gcgbarbosa <gcgbarbosa@gmail.com>
2024-03-19 18:49:12 -0300

    Add downloader

---
+++ src/pdf_http_converter.py
@@ -0,0 +1,36 @@
+from haystack.components.fetchers.link_content import LinkContentFetcher
+from haystack.components.converters import PyPDFToDocument
+
+
+class PdfHttpConverter:
+    """
+    Class that fetches PDF documents from HTTP links and converts them to text
+    using PyPDFToDocument
+    """
+    def __init__(self) -> None:
+        """
+        Create a new PDFHttpConverter instance
+        """
+        self.fetcher = LinkContentFetcher()
+        self.converter = PyPDFToDocument()
+
+    def link_to_document(self, link: str) -> None:
+        """
+        Given a link to a PDF document, fetch it and convert it to text using PyPDFToDocument.
+        The text is returned as a single string.
+
+        Parameters
+        ----------
+        link: str
+            The URL of the PDF document
+
+        Returns
+        -------
+        str
+            The text of the PDF document
+        """
+        streams = self.fetcher.run(urls=[link])["streams"]
+        text_from_pdf = self.converter.run(streams)
+        # The output of PyPDFToDocument is a dict containing a list of documents
+        # We're only interested in the first document in the list
+        return text_from_pdf['documents'][0]
